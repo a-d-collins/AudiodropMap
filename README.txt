2fa4cf64b423b8ac3b7ab600f8293abd280e5f2b
A <adcollins91@gmail.com>
2016-06-01 20:09:03 -0400

    Initial layout
    Bare-bones layout of audio drop map.
    
    TODO: Integrate Google Maps API

---
+++ README.txt
@@ -0,0 +1,5 @@
+AudiodropMap_v_1.0.0
+
+Allows user to place an Audiodrop on a map.
+
+--> Utilizes Google Maps API
---
+++ js/controllers/dropMapController.js
@@ -0,0 +1,13 @@
+app.controller('dropMapController', ['$scope', function($scope) {
+    // Form data
+    // TODO -- Get form from Seth
+    $scope.formData = [
+        {dataType: "Longitude", value: 50.0000},
+        {dataType: "Latitude", value: 50.0000},
+        {dataType: "Album Name", value: "Abbey Road"},
+        {dataType: "In your opinion, is Paul dead?", value: "Yes"}
+    ];
+    
+    // Insert AJS-adapted google maps stuff below...
+    
+}]);

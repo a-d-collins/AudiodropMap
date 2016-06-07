699a33105834f390ca59abaa99105b0dca2d784c
A <adcollins91@gmail.com>
2016-06-07 14:38:08 -0400

    Clean up AngularGMaps_1 branch
    index.html:
    1) Delete markers class example
    2) Delete rectangle class example
    
    dropMapController.js:
    1) Remove clickedMarkerWindow property from map object
    2) Delete everything to do with the map.rectangle object
    2) Delete everything to do with the markers2 object (including all
    functions)
    3) Delete all commented code from the bottom of the js file.

--- js/controllers/dropMapController.js
+++ js/controllers/dropMapController.js
@@ -24,9 +24,6 @@ app.controller('dropMapController', ['$scope', 'uiGmapLogger', 'uiGmapGoogleMapA
         ],
         events: {
             // See angular.extend method for more about the available events
-        },
-        rectangle: {
-            bounds:{}
         }
     };
     
@@ -34,22 +31,7 @@ app.controller('dropMapController', ['$scope', 'uiGmapLogger', 'uiGmapGoogleMapA
     uiGmapGoogleMapApi.then(function(maps) {
         // examine the maps object
         console.log(maps);
-        // Use maps object to set the bounds of the map.rectangle object
-        $scope.map.rectangle.bounds = new maps.LatLngBounds(
-          new maps.LatLng(55,-100),
-          new maps.LatLng(49,-78)
-        );
     });
-    
-    // Declare functions as variables that are used a lot in the javascript and do not need to be available to the $scope
-    var onMarkerClicked = function (marker) {
-        marker.showWindow = true;
-        $scope.$apply();
-    };
-    
-    // Attach (some) functions declared as 'var' variables to the $scope
-    // This might also be a useful way to 'angularize' a bunch of javascript code that someone else is working on
-    $scope.onMarkerClicked = onMarkerClicked;
     
     // This method allows you to alter/add to (a.k.a. 'extend') the properties of any earlier-declared object (e.g. $scope.map)
     // Useful for cleaning up the beginning of a JS file because you can place all of the busy details lower down in the file
@@ -78,9 +60,6 @@ app.controller('dropMapController', ['$scope', 'uiGmapLogger', 'uiGmapGoogleMapA
         clickedMarker: {
             id: 0,
             showWindow: true
-        },
-        clickedMarkerWindow: {
-            show: false
         },
         events: {
             click: function (mapModel, eventName, originalEventArgs) {
@@ -111,172 +90,7 @@ app.controller('dropMapController', ['$scope', 'uiGmapLogger', 'uiGmapGoogleMapA
                 $scope.$evalAsync();
             }
             
-        },
-        markers2: [
-            {
-                id: 1,
-                latitude: 46,
-                longitude: -77,
-                showWindow: false,
-                options: {
-                    labelContent: '[46,-77]',
-                    labelAnchor: "22 0",
-                    labelClass: "marker-labels"
-                }
-            },
-            {
-                id: 2,
-                latitude: 33,
-                longitude: -77,
-                showWindow: false,
-                options: {
-                    labelContent: 'DRAG ME!',
-                    labelAnchor: "22 0",
-                    labelClass: "marker-labels",
-                    draggable: true
-                }
-            },
-            {
-                id: 3,
-                latitude: 35,
-                longitude: -125,
-                showWindow: false,
-                options: {
-                    labelContent: '[35,-125]',
-                    labelAnchor: "22 0",
-                    labelClass: "marker-labels"
-                }
-            }
-        ],
-        rectangle: {
-            stroke: {
-                color: '#08B21F',
-                weight: 2,
-                opacity: 1
-            },
-            fill: {
-                color: 'pink',
-                opacity: 0.5
-            },
-            events:{
-                dblclick: function(){
-                    window.alert("rectangle dblclick");
-                }
-            },
-            draggable: true, // optional: defaults to false
-            clickable: true, // optional: defaults to true
-            editable: true, // optional: defaults to false
-            visible: true // optional: defaults to true
-        },
-        //zoom: 3
-    });
-    
-    // Other functions
-    $scope.map.markers2Events = {
-        dragend: function (marker, eventName, model, args) {
-            model.options.labelContent = "Dragged lat: " + model.latitude + " lon: " + model.longitude;
         }
-    };
-    
-    $scope.map.markers2.forEach( function (marker) {
-        marker.onClicked = function () {
-            onMarkerClicked(marker);
-        };
-        marker.closeClick = function () {
-            marker.showWindow = false;
-            $scope.$evalAsync();
-        };
     });
-    
-    /*$scope.addMarker = function (position) {
-        alert(position);
-    };*/
-    
-    /*$scope.marker = {
-          id: 0,
-          coords: {
-            latitude: position[0],
-            longitude: position[1]
-          },
-          options: { draggable: true },
-          events: {
-            dragend: function (marker, eventName, args) {
-              $log.log('marker dragend');
-              var lat = marker.getPosition().lat();
-              var lon = marker.getPosition().lng();
-              $log.log(lat);
-              $log.log(lon);
-
-              $scope.marker.options = {
-                draggable: true,
-                labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-                labelAnchor: "100 0",
-                labelClass: "marker-labels"
-              };
-            }
-          }
-        };*/
-    
-    
-    /*
-    GENERAL IDEA
-    Initializer.mapsInitialized.then(function(){
-        // Set center of map
-        var haightAshbury = {lat: 37.769, lng: -122.446};
-        
-        // Create map
-        $scope.map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: haightAshbury,
-            mapTypeId: google.maps.MapTypeId.TERRAIN
-        });
-        
-        // This event listener will call addMarker() when the map is clicked.
-        $scope.map.addListener('click', function(event) {
-            $scope.addMarker(event.latLng);
-        });
-    });
-    
-    // Adds a marker to the map and push to the array.
-    $scope.addMarker = function (location) {
-        var marker = new google.maps.Marker({
-            position: location,
-            map: $scope.map
-        });
-
-        // Clear markers from map, clear markers array, and push new marker (array is useful if more markers are to be added later)
-        $scope.clearMarkers();
-        $scope.markers = [];
-        $scope.markers.push(marker);
-
-        // Add info window to marker (to be used more later)
-        var infowindow = new google.maps.InfoWindow({
-            content: marker.position.lat() + ", " + marker.position.lng()
-        });
-
-        // Add click event to marker that displays infowindow
-        marker.addListener('click', function() {
-            infowindow.open($scope.map, marker);
-        });
-        
-        // Update $scope.formData lat and lng with marker position
-        $scope.formData[0].value = marker.position.lat();
-        $scope.formData[1].value = marker.position.lng();
-        // $apply() the changes.
-        $scope.$apply();
-        // TODO: CONVERT THE "addMarker()" function into a service. Services do the $apply themselves.
-    }
-    
-    // Sets the map on all markers in the array.
-    $scope.setMapOnAll = function (map) {
-      for (var i = 0; i < $scope.markers.length; i++) {
-        $scope.markers[i].setMap(map);
-      }
-    }
-
-    // Removes the markers from the map, but keeps them in the array.
-    $scope.clearMarkers = function () {
-      $scope.setMapOnAll(null);
-    }*/
     
 }]);
